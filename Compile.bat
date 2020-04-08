20ad564f044e9119e03a40c0a7c9c8e0e64eef46
[Rebzygit] <[super_renat@bk.ru]>
2020-04-08 20:39:01 +0300

    addded BiqQ parameters

--- Compile.bat
+++ Compile.bat
@@ -1,4 +1,5 @@
 del /s *.class
 javac -encoding utf8 Test.java
 javac -encoding utf8 eltech\DM2020\BigNum\BigN.java
+javac -encoding utf8 eltech\DM2020\BigNum\BigQ.java
 chcp 1251
--- eltech/DM2020/BigNum/BigQ.java
+++ eltech/DM2020/BigNum/BigQ.java
@@ -1,8 +1,24 @@
 package eltech.DM2020.BigNum;
 
 import java.util.*;
-
+/**
+	* Класс, который позволяет манипулировать с большими рациональными числами
+	* @version 0.01
+	* @author Аюпов Ренат (эйкей хулаганский пес)
+	*/
 public class BigQ
-{
+{	
+	private BigZ p = new BigZ();
+	private BigZ q = new BigZ();
 	
+	private BigZ(){}
+	/**
+	* Конструктор, с помощью которого можно ввести большое рациональное число
+	* @version 1.0
+	* @author ass
+	*/
+	
+	public BigQ(BigZ p, BigZ q){
+		//work matherfuckers
+	}
 }
